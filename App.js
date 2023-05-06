42813434f1d7241f24e1207b6ad73d2957102493
Allen Loshon <allenlosho913@gmail.com>
2023-05-05 22:45:12 -0700

    theme setter algorithm

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
-import { useState, useCallback } from "react";
+import { useState, useCallback, createContext } from "react";
 import Prompts from "./Prompts.js";
 import Browse from "./Browse.js";
 import {NavigationContainer} from '@react-navigation/native';
@@ -11,20 +11,30 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   const [promptPage, setPromptPage] = useState(true);
   const [songs, setSongs] = useState([]);
+  const [currentTheme, setCurrentTheme] = useState(themes["light"]);
+  const toggle = (theme) => {
+      setCurrentTheme(themes[theme])
+  };
+
+  const ThemeContext = createContext(currentTheme)
+
+
   const togglePages = useCallback((toggle=false) => setPromptPage(toggle), []);
   return (
-    <NavigationContainer> 
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Prompts"
-          component={<Prompts setSongs={setSongs} togglePages={togglePages} />}
-          options={{title: 'Find Songs'}}
-        />
-        <Stack.Screen name="Browser" 
-          component={<Browse songs={songs} togglePages={togglePages} />}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ThemeContext.Provider value={{currentTheme}}>
+      <NavigationContainer> 
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Prompts"
+            component={<Prompts setSongs={setSongs} togglePages={togglePages} toggleTheme={toggle} />}
+            options={{title: 'Find Songs'}}
+          />
+          <Stack.Screen name="Browser" 
+            component={<Browse songs={songs} togglePages={togglePages} />}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ThemeContext.Provider>
     
   );
 }
@@ -32,3 +42,26 @@ export default function App() {
 const styles = StyleSheet.create({
   
 });
+
+const themes = {
+  light: {
+    primary: 'rgb(120, 120, 120)',
+    secondary: 'rgb(180, 180, 200)',
+    background: 'rgb(240, 240, 240)',
+  },
+  dark: {
+    primary: 'rgb(200, 200, 200)',
+    secondary: 'rgb(100, 100, 100)',
+    background: 'rgb(40, 40, 40)',
+  },
+  cool: {
+    primary: 'rgb(30, 147, 242)',
+    secondary: 'rgb(30, 242, 147)',
+    background: 'rgb(160, 228, 228)',
+  },
+  snug: {
+    primary: 'rgb(242, 147, 30)',
+    secondary: 'rgb(147, 242, 30)',
+    background: 'rgb(228, 228, 160)',
+  },
+}
